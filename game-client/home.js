04a186a487497839e36bdd387d95921dd1a3f3c7
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2021-08-25 07:51:18 +0100

    game list includes arda

--- game-client/home.js
+++ game-client/home.js
@@ -1,3 +1,13 @@
+
+const emptyNode = function(parent)
+{
+    if (parent !== null)
+    {
+        while (parent.firstChild) 
+            parent.removeChild(parent.firstChild);
+    }
+};
+
 const isInvalid = function(name)
 {
     return name === "" || name.indexOf("<") !== -1 || name.indexOf(">") !== -1;
@@ -10,6 +20,9 @@ const randomNumber = function(max)
 
 const loadSampleRooms = function(existing)
 {
+    if (document.getElementById("enter_room").value !== "")
+        return;
+
     fetch("/data/samplerooms").then((response) => response.json().then((_rooms) => 
     {
         const filtered = _rooms.filter((candidate) => !existing.includes(candidate));
@@ -18,33 +31,89 @@ const loadSampleRooms = function(existing)
     }));
 }
 
-const onResult = function(data)
+const addGameTypes = function(data, isArda, existing)
 {
-    if (data === undefined || data.length === 0)
-    {
-        loadSampleRooms([]);
-        document.getElementById("no_games").classList.remove("hidden");
-        return;
-    }
+    let title = isArda ? "Arda" : "Standard/Dreamcard"
+    const table = document.createElement("table");
+    table.innerHTML = `<thead><tr>
+                            <th colspan="2">${title} Game</th>
+                            <th>Players</th>
+                        </tr></thead>`;
 
-    data.sort(function(a, b) { return a.room.toLowerCase().localeCompare(b.room.toLowerCase()); });
+    const container = document.createElement("tbody");
     
-    let existing = [];
-
-    let sHtml = "";
-    data.forEach(function (value)
+    let count = 0;
+    const size = data.length;
+    for (let i = 0; i < size; i++)
     {
+        let value = data[i];
+        if (isArda !== value.arda)
+            continue;
+
         let _room = value.room;
         let _players = value.players.sort().join(", ");
         let _context = value.arda ? "arda" : "play";
 
-        sHtml += `<li><a href="/${_context}/${_room}" title="Click to join this game">${_room}</a> (${_players})</li>`;
-        existing.push(_room);
-    });
+        count++;
+        const _tr = document.createElement("tr");
+        _tr.innerHTML = `<td>${count}</td><td><a href="/${_context}/${_room}" title="Click to join this game">${_room}</td><td class="players">${_players}</td>`;
+        container.appendChild(_tr);
+        existing.push(_room)
+    }
 
-    document.getElementById("game_list").innerHTML = "<ol>"+sHtml+"</ol>";
+    if (count === 0)
+        return null;
+
+    table.appendChild(container);
+    return table;
+};
+
+
+const hideContainer = function(id)
+{
+    const elem = document.getElementById(id);
+    if (elem !== null && !elem.classList.contains("hidden"))
+        elem.classList.add("hidden");
+};
+
+const showContainer = function(id)
+{
+    const elem = document.getElementById("no_games");
+    if (elem !== null && elem.classList.contains("hidden"))
+        elem.classList.remove("hidden");
+};
+
+const onResult = function(data)
+{
+    const pContainer = document.getElementById("game_list");
+    if (pContainer === null)
+        return;
+
+    emptyNode(pContainer);
+
+    if (data === undefined || data.length === 0)
+    {
+        hideContainer("active_games");
+        showContainer("no_games");
+        loadSampleRooms([]);
+        return;
+    }
+
+    data.sort(function(a, b) { return a.room.toLowerCase().localeCompare(b.room.toLowerCase()); });
+
+    let existing = [], table;
+
+    table = addGameTypes(data, false, existing);
+    if (table !== null)
+        pContainer.appendChild(table);
+
+    table = addGameTypes(data, true, existing);
+    if (table !== null)
+        pContainer.appendChild(table);
+    
     document.getElementById("active_games").classList.remove("hidden");
-
+    showContainer("active_games");
+    hideContainer("no_games");
     loadSampleRooms(existing);
 };
 
@@ -53,6 +122,15 @@ const isAlphaNumeric = function(sInput)
     return sInput !== undefined && sInput.trim() !== "" && /^[0-9a-zA-Z]{1,}$/.test(sInput);
 };
 
+const fetchAndUpdateGames = function()
+{
+    fetch("/data/games").then((response) => response.json().then(onResult))
+    .catch((err) => 
+    {
+        console.log(err);
+        document.body.dispatchEvent(new CustomEvent("meccg-notify-error", { "detail": "Could not fetch game list." }));
+    });
+};
 
 (function()
 {
@@ -93,11 +171,7 @@ const isAlphaNumeric = function(sInput)
 
     document.getElementById("enter_room").focus();
 
+    fetchAndUpdateGames();
 })();
 
-fetch("/data/games").then((response) => response.json().then(onResult))
-.catch((err) => 
-{
-    console.log(err);
-    document.body.dispatchEvent(new CustomEvent("meccg-notify-error", { "detail": "Could not fetch game list." }));
-});
+setInterval(fetchAndUpdateGames, 10 * 1000);
