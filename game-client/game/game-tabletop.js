78097aa354704d9ab9ca845bb0c1de3a55413f9a
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2021-11-19 17:36:46 +0000

    unused objects removed

--- game-client/game/game-tabletop.js
+++ game-client/game/game-tabletop.js
@@ -6,8 +6,8 @@ g_Game.onDocumentReady = function()
     g_Game.CardPreview = CardPreview;
     g_Game.CardList = new CardList();
     g_Game.TaskBarCards = new TaskBarCardsInterface(g_Game.CardList, g_Game.CardPreview);
-    g_Game.Scoring = createScoringApp(g_Game.CardList, g_Game.TaskBarCards);
-    g_Game.HandCardsDraggable = createHandCardsDraggable(g_Game.CardPreview,MeccgApi, g_Game.Scoring);
+    g_Game.Scoring = createScoringApp(g_Game.CardList);
+    g_Game.HandCardsDraggable = createHandCardsDraggable(g_Game.CardPreview,MeccgApi);
     g_Game.CompanyManager = createCompanyManager(g_Game.CardList, g_Game.CardPreview, g_Game.HandCardsDraggable);
     g_Game.Stagingarea = createStagingArea(g_Game.CardList, g_Game.CardPreview);
 
