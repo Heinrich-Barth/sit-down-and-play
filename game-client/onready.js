74d231e2132bded7d869a04e2fd0d2b4c0567b73
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2022-01-05 08:05:41 +0000

    init all after a short timeout

--- game-client/onready.js
+++ game-client/onready.js
@@ -1,1 +1,9 @@
-document.body.dispatchEvent(new CustomEvent("meccg-init-ready"));
+
+/** allow content paint */
+setTimeout(() => {
+
+    document.body.dispatchEvent(new CustomEvent("meccg-init-ready"));
+
+}, 100);
+
+
