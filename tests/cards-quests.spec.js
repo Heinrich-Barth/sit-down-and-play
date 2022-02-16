fb8d0b85e702802b6b8e4c071a563bf4b330640a
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2022-02-16 09:30:57 +0000

    test identify quest if cards-raw exists

---
+++ tests/cards-quests.spec.js
@@ -0,0 +1,39 @@
+
+const CardsQuest = require("../plugins/cards-quests");
+
+
+const readJson = function()
+{
+    try
+    {
+        const fs = require('fs');
+        return JSON.parse(fs.readFileSync(__dirname + "/../data/cards-raw.json", 'utf8'));
+    }
+    catch (e)
+    {
+        /** not available */
+    }
+
+    return [];
+}
+
+describe('Quests', () => {
+    
+    it('identifyQuests()', () => {
+
+        const json = readJson();
+        const list = CardsQuest.identifyQuests(json);
+
+        const keys = Object.keys(list);
+        if (json.length === 0)
+            expect(keys.length).toEqual(0);
+        else 
+            expect(keys.length).toBeGreaterThan(0);
+
+        for (let key of keys)
+        {
+            expect(key.length).toBeGreaterThan(0);
+            expect(list[key].length).toBeGreaterThan(0);
+        }
+    });
+});
