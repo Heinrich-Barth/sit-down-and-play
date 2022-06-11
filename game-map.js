c804b7c91e6754b675c9629a0a2322666079f734
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2022-06-11 17:12:42 +0100

    add cookie preference

--- game-map.js
+++ game-map.js
@@ -39,6 +39,7 @@ pCookiePreferences.addPreference("dwarf", true);
 pCookiePreferences.addPreference("lord", true);
 pCookiePreferences.addPreference("fallenlord", true);
 pCookiePreferences.addPreference("dragon", false);
+pCookiePreferences.addPreference("dreamcards", true);
 
 exports.setup = function(SERVER, isProduction, g_pExpress)
 {
@@ -57,8 +58,8 @@ exports.setup = function(SERVER, isProduction, g_pExpress)
     /**
      * Provide the map data with all regions and sites for the map windows
      */
-    SERVER.instance.get("/data/list/map", (req, res) => SERVER.cacheResponse(res, "application/json").send(SERVER.cards.getMapdata()).status(200));
-    SERVER.instance.get("/data/list/underdeeps", (req, res) => SERVER.cacheResponse(res, "application/json").send(SERVER.cards.getUnderdeepMapdata()).status(200));
+    SERVER.instance.get("/data/list/map", (_req, res) => SERVER.cacheResponse(res, "application/json").send(SERVER.cards.getMapdata()).status(200));
+    SERVER.instance.get("/data/list/underdeeps", (_req, res) => SERVER.cacheResponse(res, "application/json").send(SERVER.cards.getUnderdeepMapdata()).status(200));
 
     SERVER.instance.get("/data/preferences/map", (req, res) => SERVER.expireResponse(res, "application/json").send(pCookiePreferences.get(req.cookies)).status(200));
     SERVER.instance.post("/data/preferences/map", (req, res) =>  { 
