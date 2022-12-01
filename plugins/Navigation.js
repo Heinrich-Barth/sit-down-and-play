32edfc9ca3bd2a5d4229917b4a6455e310a180f4
Phillip Austefield <75542195+Heinrich-Barth@users.noreply.github.com>
2022-12-01 13:39:00 +0000

    update navigation routes

--- plugins/Navigation.js
+++ plugins/Navigation.js
@@ -1,19 +1,18 @@
-const navigationEntry = function(url, label)
+const navigationEntry = function(url, label, blank)
 {
-    return { url: url, label: label };
+    return { url: url, label: label, blank: blank };
 };
 
 
 const getNavigationJson = function(_req, res)
 {
     let targetList = [];
-    targetList.push(navigationEntry("/cards", "Card Browser"));
-    targetList.push(navigationEntry("/deckbuilder", "Deckbuilder"));
-    targetList.push(navigationEntry("/converter", "Import a deck"));
-    targetList.push(navigationEntry("/", "Play a game"));
-    targetList.push(navigationEntry("/map/regions", "Region Map"));
-    targetList.push(navigationEntry("/map/underdeeps", "Underdeeps Map"));
-    targetList.push(navigationEntry("/about", "About"));
+    targetList.push(navigationEntry("/play", "Play a game", false));
+    targetList.push(navigationEntry("/deckbuilder", "Deckbuilder", false));
+    targetList.push(navigationEntry("/converter", "Import Deck", false));
+    targetList.push(navigationEntry("/map/regions", "Region Map", true));
+    targetList.push(navigationEntry("/map/underdeeps", "Underdeeps Map", true));
+    targetList.push(navigationEntry("/about", "About", false));
     res.send(targetList).status(200)
 };
 
@@ -26,9 +25,8 @@ module.exports = function(SERVER, g_pExpress, g_pAuthentication, htmlDir)
     SERVER.instance.get("/data/navigation", SERVER.caching.cache.jsonCallback, getNavigationJson);
 
     SERVER.instance.use("/about", g_pExpress.static(htmlDir + "/pages/about.html", SERVER.caching.headerData.generic));
-    SERVER.instance.use("/converter", g_pExpress.static(htmlDir + "/pages/converter.html", SERVER.caching.headerData.generic));
+    SERVER.instance.use("/converter", SERVER.endpointVisits.increase, g_pExpress.static(htmlDir + "/pages/converter.html", SERVER.caching.headerData.generic));
     SERVER.instance.use("/help", g_pExpress.static(htmlDir + "/pages/help.html", SERVER.caching.headerData.generic));
-    SERVER.instance.use("/deckbuilder", g_pAuthentication.isSignedInDeckbuilder, g_pExpress.static(htmlDir + "/pages/deckbuilder.html", SERVER.caching.headerData.generic));
-    SERVER.instance.use("/cards", g_pAuthentication.isSignedInCards, g_pExpress.static(htmlDir + "/pages/card-browser.html", SERVER.caching.headerData.generic));
+    SERVER.instance.use("/deckbuilder", g_pAuthentication.isSignedInDeckbuilder, SERVER.endpointVisits.increase, g_pExpress.static(htmlDir + "/pages/deckbuilder.html", SERVER.caching.headerData.generic));
 
 }
