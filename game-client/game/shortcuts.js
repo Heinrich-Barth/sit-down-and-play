0016a5f058726e6bf3c7ddec2c3b91ff52724eb0
Phillip Austefield <75542195+Heinrich-Barth@users.noreply.github.com>
2023-01-02 11:02:09 +0000

    fix deprecated usage

--- game-client/game/shortcuts.js
+++ game-client/game/shortcuts.js
@@ -11,21 +11,16 @@ document.body.addEventListener("keyup", function(ev)
     {
         /* ESC */
         case "Escape":
-        case "27":
             MapWindow.close();
             break;
 
         /* R */
-        case "KeyR":
         case "r":
-        case "82":
             document.getElementById("playercard_hand").querySelector(".card-dice").dispatchEvent(new Event("click"));
             break;
 
         /* D */
-        case "KeyD":
         case "d":
-        case "68":
             document.getElementById("draw_card").click();
             break;
 
