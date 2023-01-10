c4d5385842368090ad73a44290776ccdb23399b3
Phillip Austefield <75542195+Heinrich-Barth@users.noreply.github.com>
2023-01-10 14:38:07 +0000

    check for set cookies

--- server/ClearCookies.js
+++ server/ClearCookies.js
@@ -1,21 +1,32 @@
 
-const clearCookies = function(res)
+const clearCookies = function(req, res)
 {
-    res.clearCookie('userId');
-    res.clearCookie('joined');
-    res.clearCookie('room');
-    res.clearCookie('socialMedia');
-    res.clearCookie('socialMediaPers');
+    if (req === undefined || req.cookies === undefined || req.cleared !== undefined)
+        return res;
+
+    if (req.cookies.userId !== undefined)
+        res.clearCookie('userId');
+
+    if (req.cookies.joined !== undefined)
+        res.clearCookie('joined');
+
+    if (req.cookies.room !== undefined)
+        res.clearCookie('room');
+    
+    if (req.cookies.userId !== undefined)
+        res.clearCookie('userId');
+
+    if (req.cookies.socialMediaPers !== undefined)
+        res.clearCookie('socialMediaPers');
 
     return res;
 }
 
-
 exports.clearCookies = clearCookies;
 
-exports.clearCookiesCallback = function(_req, res, next)
+exports.clearCookiesCallback = function(req, res, next)
 {
-    clearCookies(res);
+    clearCookies(req, res);
     next();
 }
 
