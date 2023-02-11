92821349cb663961a463180c2a1abf5953f70420
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2023-02-11 19:00:01 +0000

    Add map-not-found route

--- server/RoutingMap.js
+++ server/RoutingMap.js
@@ -47,6 +47,7 @@ exports.setup = function(SERVER, isProduction, g_pExpress)
 
     /* Map images should be cached */
     SERVER.instance.use("/media/maps", g_pExpress.static(__dirname + "/../media/maps", SERVER.caching.headerData.generic));
+    SERVER.instance.use("/media/map-black-tile", g_pExpress.static(__dirname + "/../media/map-tile.jpg", SERVER.caching.headerData.generic));
 
     /**
      * Show Map Pages
