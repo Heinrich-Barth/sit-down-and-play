ecbb9736fee7ed7ead2827bd4a2308ded11ffdc0
Phillip Austefield <75542195+Heinrich-Barth@users.noreply.github.com>
2023-04-04 08:24:17 +0100

    add jsdoc

--- game-management/ResultToken.js
+++ game-management/ResultToken.js
@@ -2,11 +2,21 @@ const crypto = require('crypto');
 
 const TOKEN_CHECKSUM = typeof process.env.SECRET_RESULTTOKEN !== "undefined" ? process.env.SECRET_RESULTTOKEN : "" + Date.now();
 
+/**
+ * Calculate hash
+ * @param {String} input 
+ * @returns Hash String
+ */
 const createHash = function(input)
 {
     return crypto.createHmac('sha256', TOKEN_CHECKSUM).update(input).digest('base64url')
 }
 
+/**
+ * Create JWT without signature part
+ * @param {JSON} data Payload
+ * @returns Token 
+ */
 const createPlainToken = function(data)
 {
     const header = {
@@ -18,12 +28,22 @@ const createPlainToken = function(data)
     return base64First + "." + base64Second;
 }
 
+/**
+ * Create JWT with signed part
+ * @param {JSON} data Payload
+ * @returns JWT 
+ */
 exports.create = function(data)
 {
     const first = createPlainToken(data);
     return first + "." + createHash(first);
 }
 
+/**
+ * Validate JWT
+ * @param {String} token JWT token to validate
+ * @returns boolean
+ */
 exports.validate = function(token)
 {
     if (typeof token !== "string")
@@ -32,3 +52,10 @@ exports.validate = function(token)
     const parts = token.split(".");
     return parts.length === 3 && parts[2] !== "" && parts[2] === createHash(parts[0] + "." + parts[1]);
 }
+
+/**
+ * calculate hash from input
+ * @param {String} input
+ * @returns String
+ */
+exports.createHash = createHash;
