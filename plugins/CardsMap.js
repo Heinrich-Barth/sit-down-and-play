d809d89cdcb065dbec5ba3065ba5302cf7366bcb
Heinrich Barth <75542195+Heinrich-Barth@users.noreply.github.com>
2023-11-29 10:10:45 +0000

    refractor

--- plugins/CardsMap.js
+++ plugins/CardsMap.js
@@ -1,20 +1,16 @@
-
-
+const MapData = require("./MapData");
+const MapDataUnderdeeps = require("./MapDataUnderdeeps");
+const pMapData = new MapData();
 
 module.exports = function(jsonCards, mapPositionsFile, _imageList)
 {
-    const MapData = require("./MapData");
-    const MapDataUnderdeeps = require("./MapDataUnderdeeps");
-
-    const pMapData = new MapData();
     pMapData.init(jsonCards, mapPositionsFile);
-    const jUnderdeepJson = new MapDataUnderdeeps(jsonCards).get(_imageList);
 
     return {
 
         mapdata:  pMapData.getMapdata(),
         siteList: pMapData.getSiteList(),
-        underdeeps : jUnderdeepJson
+        underdeeps : new MapDataUnderdeeps(jsonCards).get(_imageList)
         
     }
 };
